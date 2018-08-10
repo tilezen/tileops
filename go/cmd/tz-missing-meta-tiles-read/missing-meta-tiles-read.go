f7f1ea504258843f173122cedec853cb4c80f79e
Matt Amos <zerebubuth@gmail.com>
2018-08-10 15:56:44 +0100

    Add options to missing-meta-tiles-read to allow control over zoom range, and whether the list printed is the missing tiles or the present tiles.

--- go/cmd/tz-missing-meta-tiles-read/missing-meta-tiles-read.go
+++ go/cmd/tz-missing-meta-tiles-read/missing-meta-tiles-read.go
@@ -74,8 +74,8 @@ func readAllCoords(coordsChan <-chan []coord.Coord) []coord.Coord {
 	return result
 }
 
-func findMissingCoords(coordsChan <-chan []coord.Coord, doneChan chan<- interface{}) {
-	allCoordsGen := gen.NewZoomRange(0, 14)
+func findMissingCoords(coordsChan <-chan []coord.Coord, doneChan chan<- interface{}, min_zoom, max_zoom uint) {
+	allCoordsGen := gen.NewZoomRange(min_zoom, max_zoom)
 	// need to realize the channel to get all coordinates
 	// in a slice first for sorting purposes
 	coords := readAllCoords(coordsChan)
@@ -88,20 +88,38 @@ func findMissingCoords(coordsChan <-chan []coord.Coord, doneChan chan<- interfac
 	doneChan <- struct{}{}
 }
 
+func printCoords(coordsChan <-chan []coord.Coord, doneChan chan<- interface{}) {
+	for coordArray := range coordsChan {
+		for _, coord := range coordArray {
+			fmt.Println(coord)
+		}
+	}
+	doneChan <- struct{}{}
+}
+
 func main() {
 	var bucket string
 	var datePrefix string
-	var concurrency uint
+	var concurrency, min_zoom, max_zoom uint
 	var region string
+	var present bool
 
 	flag.StringVar(&bucket, "bucket", "", "s3 bucket containing tile listing from missing-meta-tiles-write command")
 	flag.StringVar(&datePrefix, "date-prefix", "", "date prefix")
 	flag.UintVar(&concurrency, "concurrency", 16, "number of goroutines listing bucket per hash prefix")
 	flag.StringVar(&region, "region", "us-east-1", "region")
+	flag.BoolVar(&present, "present", false, "If set, return tiles which are present rather than missing. The default (false) is to return tiles which are missing within the zoom range.")
+	flag.UintVar(&min_zoom, "min-zoom", 0, "Minimum zoom to check for missing tiles (inclusive). (default 0)")
+	flag.UintVar(&max_zoom, "max-zoom", 14, "Maximum zoom to check for missing tiles (inclusive).")
 
 	flag.Parse()
 
 	if bucket == "" || datePrefix == "" || concurrency == 0 {
+		cmd.DieWithUsage()
+	}
+
+	if max_zoom < min_zoom {
+		fmt.Fprintf(os.Stderr, "Max zoom must be >= min zoom.\n")
 		cmd.DieWithUsage()
 	}
 
@@ -117,6 +135,10 @@ func main() {
 
 	go listObjects(keysChan, svc, bucket, datePrefix)
 	go readKey(keysChan, coordsChan, svc, bucket, concurrency)
-	go findMissingCoords(coordsChan, doneChan)
+	if present {
+		go printCoords(coordsChan, doneChan)
+	} else {
+		go findMissingCoords(coordsChan, doneChan, min_zoom, max_zoom)
+	}
 	<-doneChan
 }
