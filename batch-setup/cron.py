81f6e82bf61b81cf392e74be1e915d04a0b4ec59
Matt Amos <zerebubuth@gmail.com>
2018-08-31 16:34:37 +0100

    Add options to configure the meta tile date prefix separately from the rest of the date prefixes. Useful when something goes wrong with the meta tile build, but the rest of the build is OK to be re-used.

--- batch-setup/cron.py
+++ batch-setup/cron.py
@@ -505,9 +505,16 @@ if __name__ == '__main__':
                         'tileops software to use on the TPS instance.')
     parser.add_argument('--metatile-size', default=8, type=int,
                         help='Metatile size (in 256px tiles).')
+    parser.add_argument('--meta-date-prefix', help='Meta tile bucket '
+                        'date prefix, defaults to planet date. You can '
+                        'also set the environment variable '
+                        'META_DATE_PREFIX.')
 
     args = parser.parse_args()
     planet_date = datetime.strptime(args.date, '%y%m%d')
+    meta_date_prefix = (args.meta_date_prefix or
+                        os.environ.get('META_DATE_PREFIX') or
+                        planet_date)
 
     region = args.region or os.environ.get('AWS_DEFAULT_REGION')
     if region is None:
@@ -584,6 +591,7 @@ if __name__ == '__main__':
         vector_datasource_version=args.vector_datasource_version,
         tileops_version=args.tileops_version,
         metatile_size=args.metatile_size,
+        meta_date_prefix=meta_date_prefix,
     )
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
